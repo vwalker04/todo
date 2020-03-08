908211c5eb024bc5dbe8e3c1b5b9bfc26461b666
Vaughn Walker <vwalker04@yahoo.com>
2020-03-07 22:23:47 -0500

    Add test for non-existing item in repository

--- src/test/java/me/vaughnwalker/todospringmavenpostgres/ItemRepositoryTest.java
+++ src/test/java/me/vaughnwalker/todospringmavenpostgres/ItemRepositoryTest.java
@@ -2,12 +2,16 @@ package me.vaughnwalker.todospringmavenpostgres;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.NoSuchElementException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 @ExtendWith(SpringExtension.class)
@@ -29,9 +33,14 @@ class ItemRepositoryTest {
         itemRepository.save(itemToSave);
 
         Item itemToRetrieve = itemRepository.findById(1L).orElseGet(() ->
-            fail("Failed to get item."));
+                fail("Expected to retrieve an item but did not."));
 
         assertThat(itemToRetrieve.getDescription()).isEqualTo(itemToSave.getDescription());
+    }
+
+    @Test
+    void exceptionThrownWhenUnableToFindById() {
+        assertThrows(NoSuchElementException.class, itemRepository.findById(Mockito.anyLong())::get);
     }
 
 
